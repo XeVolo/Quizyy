8ea88d3266b62c144ab042fa0a9733eeb294e8a5
xevolo <xevolo12@gmail.com>
2023-11-17 00:10:17 +0100

    przyciski fit 1

--- Controller/FitController.cs
+++ Controller/FitController.cs
@@ -23,10 +23,14 @@ namespace Quizyy.Controller
 		private void FitControllerService()
 		{
 			
-			int x = 20, y = 15,count=0;
+			int x = 40, y = 14,count=0;
 			FitOptionModel ?flashcard1 =null;
 			FitOptionModel ?flashcard2 = null;
+			FitOptionModel? delete1 = null;
+			FitOptionModel? delete2 = null;
 			List<FitOptionModel> optionlist = NewSet();
+			FitOptionModel current = optionlist[0];
+			int con = 0;
 			while (true)
 			{
 				Console.SetCursorPosition(x, y);
@@ -34,20 +38,139 @@ namespace Quizyy.Controller
 				switch (keyInfo.Key)
 				{
 					case ConsoleKey.LeftArrow:
-						if (x > 0)
-							x--;
+						if (current.positionx == 95)
+						{
+							foreach (var item in optionlist)
+							{
+								if (item.positiony == current.positiony && item.positionx == 40)
+								{
+									if (con != 1)
+									{
+										OptionView.NewOption(current.option.definition, current.positionx, current.positiony);
+									}
+									current = item;
+									x = 40;
+									OptionView.ChoosenOption(item.option.concept, item.positionx, item.positiony);
+									con = 0;
+									break;
+								}
+							}
+						}
 						break;
 					case ConsoleKey.RightArrow:
-						if (x < Console.WindowWidth - 1)
-							x++;
+						if (current.positionx == 40)
+						{
+							foreach(var item in optionlist)
+							{
+								
+								if (item.positiony == current.positiony&&item.positionx==95)
+								{
+									if (con != 1)
+									{
+										OptionView.NewOption(current.option.concept, current.positionx, current.positiony);
+									}
+									current = item;
+									x = 95;
+									OptionView.ChoosenOption(item.option.definition, item.positionx, item.positiony);
+									con = 0;
+									break;
+								}
+							}
+						}
 						break;
 					case ConsoleKey.UpArrow:
-						if (y > 0)
-							y--;
+						if (x == 40)
+						{
+							for (int i=optionlist.Count-1;i>=0;i--)
+							{
+								FitOptionModel item = optionlist[i];
+								if (item.positionx == 40)
+								{
+									if (item.positiony < y)
+									{
+										if (con != 1)
+										{
+											OptionView.NewOption(current.option.concept, current.positionx, current.positiony);
+										}
+										y = item.positiony;
+										current = item;
+										OptionView.ChoosenOption(item.option.concept, item.positionx, item.positiony);
+										con = 0;
+										break;
+									}
+								}
+
+							}
+						}
+						else if (x == 95)
+						{
+							for (int i = optionlist.Count - 1; i >= 0; i--)
+							{
+								FitOptionModel item = optionlist[i];
+								if (item.positionx == 95)
+								{
+									if (item.positiony < y)
+									{
+										if (con != 1)
+										{
+											OptionView.NewOption(current.option.definition, current.positionx, current.positiony);
+										}
+										y = item.positiony;
+										current = item;
+										OptionView.ChoosenOption(item.option.definition, item.positionx, item.positiony);
+										con = 0;
+										break;
+									}
+								}
+
+							}
+						}
 						break;
 					case ConsoleKey.DownArrow:
-						if (y < Console.WindowHeight - 1)
-							y++;
+						if (x == 40)
+						{
+							foreach (var item in optionlist)
+							{
+								if (item.positionx == 40) 
+								{
+									if (item.positiony > y)
+									{
+										if (con != 1)
+										{
+											OptionView.NewOption(current.option.concept, current.positionx, current.positiony);
+										}
+										y =item.positiony;
+										current= item;
+										OptionView.ChoosenOption(item.option.concept, item.positionx, item.positiony);
+										con = 0;
+										break;
+									}
+								}
+								
+							}
+						}else if (x == 95)
+						{
+							foreach (var item in optionlist)
+							{
+								if (item.positionx == 95)
+								{
+									if (item.positiony > y)
+									{
+										if (con != 1)
+										{
+											OptionView.NewOption(current.option.definition, current.positionx, current.positiony);
+										}
+										y = item.positiony;
+										current = item;
+										OptionView.ChoosenOption(item.option.definition, item.positionx, item.positiony);
+										con = 0;
+										break;
+									}
+								}
+
+							}
+						}
+
 						break;
 					case ConsoleKey.Enter:
 						foreach(var f in optionlist) {
@@ -93,8 +216,13 @@ namespace Quizyy.Controller
 											ClearFieldView.ClearField(flashcard1.positionx, flashcard1.positiony);
 											ClearFieldView.ClearField(flashcard2.positionx, flashcard2.positiony);
 											count++;
+											delete1 = flashcard1;
+											delete2= flashcard2;
+											
 											flashcard1 = null;
 											flashcard2 = null;
+											con = 1;
+											
 										}
 										else
 										{
@@ -109,6 +237,11 @@ namespace Quizyy.Controller
 								break;
 							}
 						}
+
+						optionlist.Remove(delete1);
+						optionlist.Remove(delete2);
+						delete1 = null;
+						delete2 = null;
 						if (count == 7) CreateFitController();
 						break;
 					case ConsoleKey.Escape:
@@ -151,7 +284,7 @@ namespace Quizyy.Controller
 
 			List<int> drawn = GetNumbers(xy);
 
-			OptionView.NewOption(lista[drawn[0]].concept, 40, 14);
+			OptionView.ChoosenOption(lista[drawn[0]].concept, 40, 14);
 			optionlist.Add(new FitOptionModel(0, lista[drawn[0]], 40, 14, lista[drawn[0]].concept.Length));
 			OptionView.NewOption(lista[drawn[1]].concept, 40, 17);
 			optionlist.Add(new FitOptionModel(1, lista[drawn[1]], 40, 17, lista[drawn[1]].concept.Length));
