f1b668f7281e7d685a43dc6e47506bdef299b65a
xevolo <xevolo12@gmail.com>
2023-11-17 02:18:00 +0100

    chyba gotowe

--- Controller/ChooseController.cs
+++ Controller/ChooseController.cs
@@ -22,8 +22,8 @@ namespace Quizyy.Controller
 		}
 		private void ChooseControllerService()
 		{
-			int x = 75, y = 25;
-			FitOptionModel ansplace= NewSet();			
+			int x = 40, y = 26;
+			List<FitOptionModel> optionlist = NewSet();
 			while (true)
 			{
 				Console.SetCursorPosition(x, y);
@@ -31,27 +31,73 @@ namespace Quizyy.Controller
 				switch (keyInfo.Key)
 				{
 					case ConsoleKey.LeftArrow:
-						if (x > 0)
-							x--;
+						if (x == 95 && y == 26)
+						{
+							OptionView.NewOption(optionlist[1].option.concept, optionlist[1].positionx, optionlist[1].positiony);
+							x = 40;
+							OptionView.ChoosenOption(optionlist[0].option.concept, optionlist[0].positionx, optionlist[0].positiony);
+						}
+						else if (x == 95 && y == 29)
+						{
+							OptionView.NewOption(optionlist[3].option.concept, optionlist[3].positionx, optionlist[3].positiony);
+							x = 40;
+							OptionView.ChoosenOption(optionlist[2].option.concept, optionlist[2].positionx, optionlist[2].positiony);
+						}
 						break;
 					case ConsoleKey.RightArrow:
-						if (x < Console.WindowWidth - 1)
-							x++;
+						if (x == 40 && y == 26)
+						{
+							OptionView.NewOption(optionlist[0].option.concept, optionlist[0].positionx, optionlist[0].positiony);
+							x = 95;
+							OptionView.ChoosenOption(optionlist[1].option.concept, optionlist[1].positionx, optionlist[1].positiony);
+						}
+						else if (x == 40 && y == 29)
+						{
+							OptionView.NewOption(optionlist[2].option.concept, optionlist[2].positionx, optionlist[2].positiony);
+							x = 95;
+							OptionView.ChoosenOption(optionlist[3].option.concept, optionlist[3].positionx, optionlist[3].positiony);
+						}
+
 						break;
 					case ConsoleKey.UpArrow:
-						if (y > 0)
-							y--;
+						if (x == 40 && y == 29)
+						{
+							OptionView.NewOption(optionlist[2].option.concept, optionlist[2].positionx, optionlist[2].positiony);
+							y = 26;
+							OptionView.ChoosenOption(optionlist[0].option.concept, optionlist[0].positionx, optionlist[0].positiony);
+						}
+						else if (x == 95 && y == 29)
+						{
+							OptionView.NewOption(optionlist[3].option.concept, optionlist[3].positionx, optionlist[3].positiony);
+							y = 26;
+							OptionView.ChoosenOption(optionlist[1].option.concept, optionlist[1].positionx, optionlist[1].positiony);
+						}
 						break;
 					case ConsoleKey.DownArrow:
-						if (y < Console.WindowHeight - 1)
-							y++;
+						if (x == 40 && y == 26)
+						{
+							OptionView.NewOption(optionlist[0].option.concept, optionlist[0].positionx, optionlist[0].positiony);
+							y = 29;
+							OptionView.ChoosenOption(optionlist[2].option.concept, optionlist[2].positionx, optionlist[2].positiony);
+						}
+						else if (x == 95 && y == 26)
+						{
+							OptionView.NewOption(optionlist[1].option.concept, optionlist[1].positionx, optionlist[1].positiony);
+							y = 29;
+							OptionView.ChoosenOption(optionlist[3].option.concept, optionlist[3].positionx, optionlist[3].positiony);
+						}
 						break;
 					case ConsoleKey.Enter:
-						if (x >= ansplace.positionx && x <= (ansplace.positionx + ansplace.option.concept.Length) && y >= ansplace.positiony && y <= ansplace.positiony + 2)
+						int correct = optionlist[0].length;
+						if (x >= optionlist[correct].positionx && x <= (optionlist[correct].positionx + optionlist[correct].option.concept.Length) && y >= optionlist[correct].positiony && y <= optionlist[correct].positiony + 2)
 						{
 							Console.SetCursorPosition(70, 36);
 							Console.Write("Odpowiedź poprawna");
-							ansplace = NewSet();
+							
+							optionlist = NewSet();
+							x = 40;
+							y = 26;
+							
 						}
 						else
 						{
@@ -94,7 +140,7 @@ namespace Quizyy.Controller
 			int result = rnd.Next(size);
 			return result;
 		}
-		private FitOptionModel NewSet()
+		private List<FitOptionModel> NewSet()
 		{
 			List<WriteModel> list = BaseController.GetWriteList();
 			List<string> anslist = new List<string>();
@@ -111,27 +157,36 @@ namespace Quizyy.Controller
 			ChooseView.ChooseOption(list[questionid].question);
 			List<int> drawn = GetNumbers();
 				
-				OptionView.NewOption(anslist[drawn[0]], 40, 26);
-				OptionView.NewOption(anslist[drawn[1]], 40, 29);
-				OptionView.NewOption(anslist[drawn[2]], 95, 26);
+				OptionView.ChoosenOption(anslist[drawn[0]], 40, 26);
+				OptionView.NewOption(anslist[drawn[1]], 95, 26);
+				OptionView.NewOption(anslist[drawn[2]], 40, 29);
 				OptionView.NewOption(anslist[drawn[3]], 95, 29);
+			List<FitOptionModel> optionlist = new List<FitOptionModel>();
 
+
+			int correct = 0;
 			if (drawn[0] == 0)
 			{
-				return new FitOptionModel(0, new FlashCardsModel { id = 0, concept=list[questionid].answer, definition="" }, 40, 26,2);
-			}else if (drawn[1] == 0)
+				correct = 0;
+			}
+			else if (drawn[1] == 0)
 			{
-				return new FitOptionModel(0, new FlashCardsModel { id = 0, concept = list[questionid].answer, definition = "" }, 40, 29, 2);
-			}else if (drawn[2] == 0)
+				correct = 1;
+			}
+			else if (drawn[2] == 0)
 			{
-				return new FitOptionModel(0, new FlashCardsModel { id = 0, concept = list[questionid].answer, definition = "" }, 95, 26, 2);
+				correct = 2;
 			}
 			else
 			{
-				return new FitOptionModel(0, new FlashCardsModel { id = 0, concept = list[questionid].answer, definition = "" }, 95, 29, 2);
+				correct = 3;
 			}
+			optionlist.Add(new FitOptionModel(0, new FlashCardsModel { id = 0, concept = anslist[drawn[0]], definition = "" }, 40, 26, correct-1));
+			optionlist.Add(new FitOptionModel(1, new FlashCardsModel { id = 1, concept = anslist[drawn[1]], definition = "" }, 95, 26, correct-1));
+			optionlist.Add(new FitOptionModel(2, new FlashCardsModel { id = 2, concept = anslist[drawn[2]], definition = "" }, 40, 29, correct - 1));
+			optionlist.Add(new FitOptionModel(3, new FlashCardsModel { id = 3, concept = anslist[drawn[3]], definition = "" }, 95, 29, correct - 1));
 
-
+			return optionlist;
 		}
 	}
 }
